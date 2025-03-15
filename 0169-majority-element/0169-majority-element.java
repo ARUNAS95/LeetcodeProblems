85f82b9431e2b3b032495e282ecb9f66e4157c2c
ARUNAS95 <148276101+ARUNAS95@users.noreply.github.com>
2025-03-14 23:34:57 -0400

    Time: 4 ms (51.61%), Space: 53 MB (65.46%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int size = nums.length/2;
+        return nums[size];
+    }
+}
