4a7cff324435006080b0530d9b2be555eed39dfd
ARUNAS95 <148276101+ARUNAS95@users.noreply.github.com>
2025-04-18 17:22:35 -0400

    Time: 1 ms (29.18%), Space: 42.7 MB (8.6%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int check = nums1.length-n;    
+    int c=0;
+    for (int i=check;i<nums1.length;i++){
+        nums1[i]= nums2[c];
+        c++;
+    }
+    Arrays.sort(nums1);
+    }
+}
