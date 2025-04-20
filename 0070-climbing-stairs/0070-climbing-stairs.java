0fd18323339d4671200cb62aaeda92d6f9ee1816
ARUNAS95 <148276101+ARUNAS95@users.noreply.github.com>
2025-04-20 13:07:39 -0400

    Time: 0 ms (100%), Space: 40.2 MB (81.79%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    static Map<Integer, Integer> memo = new HashMap<>();
+    public static int climbStairs(int n) {
+        
+        
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+        if(n<=1){
+            return 1;
+        }
+       
+
+        int result= (climbStairs(n-1)+ climbStairs(n-2));
+        memo.put(n,result);
+
+         return result;
+    }
+}
