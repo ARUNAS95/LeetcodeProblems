08e2b4f4d753464b1c33dbb461ec825eab4e68a9
Aruna Senthamaraikannan <148276101+ARUNAS95@users.noreply.github.com>
2026-08-13 19:29:35 -0400

    Time: 1 ms (80.01%), Space: 43.7 MB (44.57%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -16,43 +16,44 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
+
         if(root == null){
             return ans;
         }
+        boolean zigzag = false;
         Deque<TreeNode> q = new LinkedList<>();
-        boolean isZigZag = false;
-        q.add(root);
+        q.offer(root);
+
+       
         while(!q.isEmpty()){
+        List<Integer> arr = new ArrayList<>();
         int level = q.size();
-        List<Integer> arr = new ArrayList<>(level);
-        
         for(int i=0;i<level;i++){
-            
-            if(!isZigZag){
+            if(!zigzag){
                 TreeNode node = q.pollFirst();
                 arr.add(node.val);
-                if(node.left!= null){
+                if(node.left != null){
                     q.addLast(node.left);
                 }
                 if(node.right != null){
                     q.addLast(node.right);
                 }
-                
-            } else{
+            }
+            else {
                 TreeNode node = q.pollLast();
                 arr.add(node.val);
-                if(node.right!= null){
+                if(node.right != null){
                     q.addFirst(node.right);
                 }
                 if(node.left != null){
                     q.addFirst(node.left);
                 }
             }
-           
         }
-        isZigZag = !isZigZag;
-        ans.add(arr);     
+        zigzag = !zigzag;
+        ans.add(arr);
         }
+
         return ans;
     }
 }
